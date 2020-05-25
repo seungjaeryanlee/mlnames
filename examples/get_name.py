ec5abeb6ec9ef0921b31b3054f892034591f2642
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-26 08:01:15 +0900

    Update example script to discuss separated RNG

--- examples/get_name.py
+++ examples/get_name.py
@@ -1,10 +1,27 @@
+import random
+
 import mlnames
 
 
 def main():
+    # Basic functionality
     print(mlnames.get_name())
     print(mlnames.get_name(n_words=2))
     print(mlnames.get_name(prefix="1-", suffix="-2"))
+
+    # Separate seeding from random module
+    random.seed(1)
+    print(mlnames.get_name())
+
+    random.seed(1)
+    print(mlnames.get_name())
+
+    # You can explicitly set seed
+    mlnames.set_seed(1)
+    print(mlnames.get_name())
+
+    mlnames.set_seed(1)
+    print(mlnames.get_name())
 
 
 if __name__ == "__main__":
