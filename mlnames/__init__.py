a40ba74e424406934f58fe13ce729c71910d3a55
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-26 07:51:17 +0900

    Allow specifying prefix or suffic

--- mlnames/__init__.py
+++ mlnames/__init__.py
@@ -5,5 +5,5 @@ with open("corpus", "r") as f:
     corpus = [vocab.rstrip() for vocab in f.readlines()]
 
 
-def get_name(n_words=1):
-    return "-".join(random.sample(corpus, n_words))
+def get_name(n_words=1, prefix="", suffix=""):
+    return prefix + "-".join(random.sample(corpus, n_words)) + suffix
