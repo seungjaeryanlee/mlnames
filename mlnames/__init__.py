b92265130aafcb01e9a2c65d74006fab5c48f0e6
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-26 08:00:36 +0900

    Separate random number generator from default random module

--- mlnames/__init__.py
+++ mlnames/__init__.py
@@ -2,9 +2,15 @@ import os
 import random
 
 
+rng = random.Random()
+
 with open(os.path.join(os.path.dirname(__file__), "corpus"), "r") as f:
     corpus = [vocab.rstrip() for vocab in f.readlines()]
 
 
+def set_seed(seed):
+    rng.seed(seed)
+
+
 def get_name(n_words=1, prefix="", suffix=""):
-    return prefix + "-".join(random.sample(corpus, n_words)) + suffix
+    return prefix + "-".join(rng.sample(corpus, n_words)) + suffix
