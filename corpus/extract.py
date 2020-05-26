106b6b3bfb9ea5fd1c9c4abcb9fac04712b2d354
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-26 09:53:01 +0900

    Allow multiple API calls to arXiv

--- corpus/extract.py
+++ corpus/extract.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import os
+import time
 import urllib.request
 
 from bs4 import BeautifulSoup
@@ -22,18 +23,23 @@ lemmatizer = WordNetLemmatizer()
 
 
 def main():
-    url = "http://export.arxiv.org/api/query?search_query=cat:stat.ML+OR+cat:cs.AI+OR+cat:cs.LG&start=0&max_results=2000"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-    soup = BeautifulSoup(html, features="lxml")
-
+    # Populate counter
     counter = Counter()
-    for summary in tqdm(soup.findAll("summary")):
-        words = nltk.word_tokenize(summary.text)
-        words = [lemmatizer.lemmatize(word.lower()) for word in words if word.lower() not in en_stopwords and word.isalpha()]
-        tagged = nltk.pos_tag(words)
-        words = [word[0] for word in tagged if word[1] in ["NN", "NNS", "NNP", "NNPS", "FW"]]
-        counter += Counter(words)
+    for i in tqdm(range(2)):
+        url = f"http://export.arxiv.org/api/query?search_query=cat:stat.ML+OR+cat:cs.AI+OR+cat:cs.LG&start={2000*i}&max_results={2000*(i+1)}"
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+        soup = BeautifulSoup(html, features="lxml")
+
+        for summary in soup.findAll("summary"):
+            words = nltk.word_tokenize(summary.text)
+            words = [lemmatizer.lemmatize(word.lower()) for word in words if word.lower() not in en_stopwords and word.isalpha()]
+            tagged = nltk.pos_tag(words)
+            words = [word[0] for word in tagged if word[1] in ["NN", "NNS", "NNP", "NNPS", "FW"]]
+            counter += Counter(words)
+
+        # To give time for arXiv API
+        time.sleep(3)
 
     common_words_with_freq = counter.most_common(1000)
     common_words = [p[0] for p in common_words_with_freq]
